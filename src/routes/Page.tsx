0ad32585174e8fd850611f2da3b74d7c731d5111
youxfans <412386861@qq.com>
2019-07-12 16:54:07 +0800

    decorator

--- src/routes/Page.tsx
+++ src/routes/Page.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+function testable(isLoading: boolean) {
+  // props.isLoading = true;
+  return function(target: { isLoading: boolean; }) {
+    target.isLoading = isLoading;
+  }
+}
+
+@testable(true)
+class MyTestableClass {
+  static isLoading: any;
+}
+
+@testable(false)
+class MyClass {
+  static isLoading: any;
+}
+
 export default function Page () {
+  console.log(MyTestableClass.isLoading);
+  console.log(MyClass.isLoading);
   return (
-    <h1>Page</h1>
+    <div>
+      <h1>Page</h1>
+      <div>
+        认识自己的不足
+      </div>
+    </div>
   )
 }
